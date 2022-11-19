1f4ed49d437523574f467ff56a25c6c445115acf
Catalina <202104665@alu.comillas.edu>
2022-11-19 19:34:14 +0100

    Modificamos la practica 1_1 añadiendole un nuevo repositorio donde buscamos leaks.

--- Practicas/Bloque1/Bloque1_1/buscador_leaks.py
+++ Practicas/Bloque1/Bloque1_1/buscador_leaks.py
@@ -3,28 +3,17 @@
 
 '''
 Practica 1.
-Hacer un buscador de leaks en commits de github, para ello hay que usar pandas
-y expresiones regulares además del arquetipo que hemos trabajado en clase.
-
-Opcional:
-Montarlo todo en una imagen docker y hacer una barra de progreso para ver que
-esta haciendo nuestra ETL.
-'''
-
-
-'''
-YO: Bug hunter
-QUIERO: Crear un proceso ETL
-PARA: Encontrar leaks en commits
+Exportar los datos obtenidos en la practica de obtencion de leaks en commit
+de git a un fichero json.
 '''
 
 from git import Repo
 import pandas as pd
 import re, signal, sys
 
-def Extract(directorio_repositorio):
+def Extract(directorio_repositorio, main_branch):
     repo = Repo(directorio_repositorio)
-    lista_commits = list(repo.iter_commits('develop'))
+    lista_commits = list(repo.iter_commits(main_branch))
     return lista_commits
 
 def Transform(lista_commits):
@@ -34,17 +23,20 @@ def Transform(lista_commits):
     df_commits =  pd.DataFrame(list(zip(lista_commits, mensajes_commits)), columns=['Commit', 'Mensaje'])
     return df_commits
 
-def Load(df):
-    Possibles_leaks = ['password', 'pwd', 'contraseña', 'contra&ntildea','key']
+def Load(df, nombre_repo):
+    GREEN = "\033[38;5;40m"
+    BLUE = "\033[5;36m"
+    END = "\033[m"
+    Possibles_leaks = ['password', 'pwd', 'contraseña', 'contrase&ntildea', 'key']
     leaks = []
     for i in range(len(df)):
         mensaje = df['Mensaje'][i]
         id_commit = df['Commit'][i]
         for leak in Possibles_leaks:
             if re.search(leak, mensaje, re.IGNORECASE):
-                print(f'Se ha encontrado un leak en el commit {id_commit}.')
+                print('Se ha encontrado un leak en el commit ' + BLUE + f'{id_commit}' + END + '.')
                 leaks.append(id_commit)
-    print('Se ha acabado la busqueda de leaks en los commits del repositorio.')
+    print(GREEN + f'Se ha acabado la busqueda de leaks en los commits del repositorio {nombre_repo}.\n' + END)
     return leaks
 
 
@@ -56,7 +48,14 @@ signal.signal(signal.SIGINT, handler_signal)
 
 
 if __name__ == '__main__':
-    directorio_repo = './skale/skale-manager'
-    lista_commits = Extract(directorio_repo)
-    df_commits = Transform(lista_commits)
-    leaks = Load(df_commits)
+    directorio_repo1 = './skale/skale-manager'
+    main_branch1 = 'develop'
+    directorio_repo2 = './django-redis'
+    main_branch2 = 'master'
+    lista_commits1 = Extract(directorio_repo1, main_branch1)
+    df_commits1 = Transform(lista_commits1)
+    dict_leaks1 = Load(df_commits1, 'skale-manage')
+
+    lista_commits2 = Extract(directorio_repo2, main_branch2)
+    df_commits2 = Transform(lista_commits2)
+    dict_leaks2 = Load(df_commits2, 'django-redis')
