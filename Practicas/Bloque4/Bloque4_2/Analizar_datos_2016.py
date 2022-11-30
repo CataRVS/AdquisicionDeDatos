c7782e3a56fbd03cbe392aae3dfa73d40c1a085d
Catalina <202104665@alu.comillas.edu>
2022-11-30 21:35:45 +0100

    Modificamos la practica 2 del bloque 4

--- Practicas/Bloque4/Bloque4_2/Analizar_datos_2016.py
+++ Practicas/Bloque4/Bloque4_2/Analizar_datos_2016.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import re
 
 
@@ -25,6 +27,7 @@ def fusionar_dataframes(order_details: pd.DataFrame, orders: pd.DataFrame) -> pd
     tipos_pizza = list()
     tamano_pizzas = list()
     fechas = list()
+    numero_semana = list()
     dia_semana = list()
     numero_mes = list()
     horas = list()
@@ -104,6 +107,7 @@ def fusionar_dataframes(order_details: pd.DataFrame, orders: pd.DataFrame) -> pd
                     fecha_linea = datetime.strptime(fecha_str + ' 11:30:00', '%d/%m/%Y %H:%M:%S')
 
         fechas.append(fecha_linea)
+        numero_semana.append(int((fecha_linea).strftime('%W')))
         dia_semana.append((fecha_linea).strftime('%A'))
         numero_mes.append((fecha_linea).strftime('%B'))
         horas.append(int((fecha_linea).strftime('%H')))
@@ -115,10 +119,86 @@ def fusionar_dataframes(order_details: pd.DataFrame, orders: pd.DataFrame) -> pd
     date_order_details.insert(3, 'pizza_size', tamano_pizzas, True)
     date_order_details['quantity'] = cantidades
     date_order_details['date'] = fechas
+    date_order_details['week number'] = numero_semana
     date_order_details['week day'] = dia_semana
     date_order_details['month'] = numero_mes
     date_order_details['hour'] = horas
     return date_order_details
+
+
+def pizzas_type_df_a_dict(pizza_types):
+    pizza_types_dict = dict()
+    for i in range(len(pizza_types)):
+        pizza_types_dict[pizza_types.loc[i, 'pizza_type_id']] = pizza_types.loc[i, 'ingredients']
+    return pizza_types_dict
+
+
+def que_ingredientes_hay(pizza_types):
+    ingredientes = list()
+    for i in range(len(pizza_types)):
+        ingredientes_pizza = pizza_types.loc[i, 'ingredients']
+        lista_ingredientes_pizza = ingredientes_pizza.split(', ')
+        for ingrediente in lista_ingredientes_pizza:
+            if not ingrediente in ingredientes:
+                ingredientes.append(ingrediente)
+    return ingredientes
+
+
+def contar_y_guardar_ingredientes_semana(df_semana, df_ingredientes, pizza_types_dict):
+    dict_ingredientes = dict()
+    for i in range(len(df_semana)):
+        pizza = df_semana.loc[i, 'pizza_type_id']
+        ingredientes_pizza_str = pizza_types_dict[pizza]
+        ingredientes_pizza_list = ingredientes_pizza_str.split(', ')
+        cantidad = df_semana.loc[i, 'quantity']
+        tamano_letra = df_semana.loc[i, 'pizza_size']
+        if tamano_letra == 's':
+            tamano = 1
+        elif tamano_letra == 'm':
+            tamano = 2
+        elif tamano_letra == 'l':
+            tamano = 3
+        elif tamano_letra == 'xl':
+            tamano = 4
+        elif tamano_letra == 'xxl':
+            tamano = 5
+        total = tamano*cantidad
+        for ingrediente_pizza in ingredientes_pizza_list:
+            if ingrediente_pizza in dict_ingredientes.keys():
+                dict_ingredientes[ingrediente_pizza] += total
+            else:
+                dict_ingredientes[ingrediente_pizza] = total
+    df_ingredientes_semana_i = pd.DataFrame(dict_ingredientes, [df_semana.loc[i, 'week number']])
+    df_ingredientes = pd.concat([df_ingredientes, df_ingredientes_semana_i], sort=False)
+
+    return df_ingredientes
+
+
+def analizar_ingredientes_por_semana(df, df_ingredientes, pizza_types_dict):
+    for i in range(1, 52):
+        df_semana = df[df['week number'] == i].reset_index()
+        df_ingredientes = contar_y_guardar_ingredientes_semana(df_semana, df_ingredientes, pizza_types_dict)
+    return df_ingredientes
+
+
+def contar_ingredientes_pizzas_y_dibujar(pizza_types: pd.DataFrame, date_order_details: pd.DataFrame):
+    pizza_types_dict = pizzas_type_df_a_dict(pizza_types)
+    ingredientes = que_ingredientes_hay(pizza_types)
+    df_ingredientes = pd.DataFrame(columns=ingredientes)
+
+    df_ingredientes = analizar_ingredientes_por_semana(date_order_details, df_ingredientes, pizza_types_dict)
+    df_ingredientes = df_ingredientes.fillna(0)
+    df_tot_ingredientes = df_ingredientes.sum()
+    df_tot_ingredientes = pd.DataFrame(df_tot_ingredientes).reset_index()
+    df_tot_ingredientes.columns = ['Ingredient', 'Quantity']
+
+    sns.set_style("whitegrid")
+    sns.catplot(y="Ingredient", x="Quantity", kind='bar', data=df_tot_ingredientes, orient='h', height=10, aspect=0.75, color='#1f77b4')
+    plt.xlabel('Cantidad usada', fontweight="bold")
+    plt.ylabel('Ingrediente', fontweight="bold")
+    plt.savefig('ingredientes_usados.png', bbox_inches='tight')
+
+    return df_tot_ingredientes
 
 
 def contar_tipos_pizzas(pizza_types: pd.DataFrame, df_contar: pd.DataFrame) -> pd.DataFrame:
@@ -140,6 +220,24 @@ def contar_tipos_pizzas(pizza_types: pd.DataFrame, df_contar: pd.DataFrame) -> p
 
     df_tot = df_tot.sort_values('cantidad', ascending=False)
     df_tot = (df_tot.reset_index()).drop(columns=['index'])
+
+    return df_tot
+
+
+def contar_tamanos_pizzas(df_contar: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Creamos un diccionario con clave el nombre de las pizzas y con valor la cantidad de pizzas inicializado a 0
+    '''
+    lista_pizzas_tamanos = ['s', 'm', 'l', 'xl', 'xxl']
+    cuantas_pizzas_de_cada = list()
+    df_tot = pd.DataFrame()
+    for tamano in lista_pizzas_tamanos:
+        df_pizza = df_contar[df_contar['pizza_size'] == tamano]
+        cuantas_pizzas = int(df_pizza['quantity'].sum())
+        cuantas_pizzas_de_cada.append(cuantas_pizzas)
+
+    df_tot['tamanos'] = lista_pizzas_tamanos
+    df_tot['cantidad'] = cuantas_pizzas_de_cada
 
     return df_tot
 
@@ -188,7 +286,7 @@ def Extract(nombres, separadores):
     return dataframes[0], dataframes[1], dataframes[2]
 
 
-def Transform(order_details, orders):
+def Transform(pizza_types, order_details, orders):
     order_details_ordenado, orders_ordenado = ordenar_dataframes(order_details, orders)
     order_details_ordenado['quantity'].fillna(value=1, inplace=True)
     order_details_ordenado['pizza_id'].fillna(value='empty_order_v', inplace=True)
@@ -201,8 +299,10 @@ def Transform(order_details, orders):
 
 def Load(pizza_types, date_order_details):
 
-    ventas_tipos_pizza = contar_tipos_pizzas(pizza_types, date_order_details)
     ventas_dia_semana = separar_por_dia_semana(date_order_details)
     ventas_mes = separar_por_mes(date_order_details)
     ventas_hora = separar_por_hora(date_order_details)
-    return ventas_tipos_pizza, ventas_dia_semana, ventas_mes, ventas_hora
+    ingredientes_usados = contar_ingredientes_pizzas_y_dibujar(pizza_types, date_order_details)
+    ventas_tipos_pizza = contar_tipos_pizzas(pizza_types, date_order_details)
+    ventas_tamanos_pizza = contar_tamanos_pizzas(date_order_details)
+    return ventas_dia_semana, ventas_mes, ventas_hora, ingredientes_usados, ventas_tipos_pizza, ventas_tamanos_pizza
--- Practicas/Bloque4/Bloque4_2/Bloque4_2.py
+++ Practicas/Bloque4/Bloque4_2/Bloque4_2.py
@@ -16,7 +16,9 @@ from xlsxwriter.utility import xl_rowcol_to_cell
 
 
 
-def escribir_tabla_y_grafico(excel, worksheet, df: pd.DataFrame, nombre_hoja, eje_x):
+def escribir_tabla_y_grafico(excel, worksheet, df: pd.DataFrame, nombre_hoja, eje_x, titulo, fila):
+    titulo2 = excel.add_format({'bold': True, 'font_size':16, 'font_color':'blue', 'align':'center', 'border': 2})
+    worksheet.merge_range(fila+2, 1, fila+2, 14, titulo, titulo2)
     columnas = list(df.columns)
     titulo_celda = excel.add_format({'bold': True, 'border': 1})
     celda_verde = excel.add_format({'font_color':'28B463', 'border': 1})
@@ -24,52 +26,95 @@ def escribir_tabla_y_grafico(excel, worksheet, df: pd.DataFrame, nombre_hoja, ej
     celda_azul = excel.add_format({'font_color':'16B1DF', 'border': 1})
     media = df[columnas[1]].mean()
 
-    worksheet.write(4, 1, columnas[0], titulo_celda)
-    worksheet.write(4, 2, columnas[1], titulo_celda)
+    worksheet.write(fila+4, 1, columnas[0], titulo_celda)
+    worksheet.write(fila+4, 2, columnas[1], titulo_celda)
     for j in range(len(df)):
         if df.loc[j, columnas[1]] > media:
-            worksheet.write(j+5, 1, df.loc[j, columnas[0]], celda_verde)
-            worksheet.write(j+5, 2, df.loc[j, columnas[1]], celda_verde)
+            worksheet.write(fila+j+5, 1, df.loc[j, columnas[0]], celda_verde)
+            worksheet.write(fila+j+5, 2, df.loc[j, columnas[1]], celda_verde)
         elif df.loc[j, columnas[1]] < media:
-            worksheet.write(j+5, 1, df.loc[j, columnas[0]], celda_roja)
-            worksheet.write(j+5, 2, df.loc[j, columnas[1]], celda_roja)
+            worksheet.write(fila+j+5, 1, df.loc[j, columnas[0]], celda_roja)
+            worksheet.write(fila+j+5, 2, df.loc[j, columnas[1]], celda_roja)
         else:
-            worksheet.write(j+5, 1, df.loc[j, columnas[0]], celda_azul)
-            worksheet.write(j+5, 2, df.loc[j, columnas[1]], celda_azul)
-    worksheet.autofilter(f'{xl_rowcol_to_cell(4,1)}:{xl_rowcol_to_cell(4, 2)}')
+            worksheet.write(fila+j+5, 1, df.loc[j, columnas[0]], celda_azul)
+            worksheet.write(fila+j+5, 2, df.loc[j, columnas[1]], celda_azul)
 
     chart = excel.add_chart({'type': 'column'})
-    chart.set_size({'width': 720, 'height': 576})
+    chart.set_size({'width': 705, 'height': 580})
     chart.set_x_axis({'name': eje_x})
     chart.set_y_axis({'name': 'Nº de ventas'})
 
     chart.add_series({
-    'categories': f'={nombre_hoja}!$B$6:$B${j+6}',
-    'values':     f'={nombre_hoja}!$C$6:$C${j+6}'})
+    'categories': f'={nombre_hoja}!$B${fila+6}:$B${fila+j+6}',
+    'values':     f'={nombre_hoja}!$C${fila+6}:$C${fila+j+6}'})
     chart.set_legend({'none': True})
 
     # Insert the chart into the worksheet.
-    worksheet.insert_chart('E6', chart)
-    return worksheet
+    worksheet.insert_chart(f'{xl_rowcol_to_cell(fila + 4 ,4)}', chart)
+    if j < 30:
+        fila += 33
+    else:
+        fila += j + 6
+    return worksheet, fila
 
-
-def hoja(excel: xlsxwriter.Workbook, ventas_tipos_pizza: pd.DataFrame, nombre_hoja, titulo, eje_x):
-    worksheet = excel.add_worksheet(nombre_hoja)
+def hoja1(excel: xlsxwriter.Workbook, ventas_hora: pd.DataFrame, ventas_dia_semana: pd.DataFrame, ventas_mes: pd.DataFrame):
+    worksheet = excel.add_worksheet('reporte_ejecutivo_ventas')
     worksheet.merge_range(0, 0, 0, 2, 'Catalina Royo-Villanova Seguí')
     titulo1 = excel.add_format({'bold': True, 'font_size':20, 'font_color':'red', 'align':'center', 'border': 5})
-    worksheet.merge_range(1, 1, 1, 9, "Reporte Evecutivo Ventas Maven's Pizza".upper(), titulo1)
-    titulo2 = excel.add_format({'bold': True, 'font_size':16, 'font_color':'blue', 'align':'center', 'border': 2})
-    worksheet.merge_range(2, 1, 2, 9, titulo, titulo2)
-    worksheet = escribir_tabla_y_grafico(excel, worksheet, ventas_tipos_pizza, nombre_hoja, eje_x)
+    worksheet.merge_range(1, 1, 1, 14, "Reporte Ejecutivo Ventas de Maven's Pizza".upper(), titulo1)
+    worksheet, fila = escribir_tabla_y_grafico(excel, worksheet, ventas_hora, 'reporte_ejecutivo_ventas', 'Horas', 'Pizzas vendidas por hora', 0)
+    worksheet, fila = escribir_tabla_y_grafico(excel, worksheet, ventas_dia_semana, 'reporte_ejecutivo_ventas', 'Día de la semana', 'Pizzas vendidas por día de la semana', fila)
+    worksheet, fila = escribir_tabla_y_grafico(excel, worksheet, ventas_mes, 'reporte_ejecutivo_ventas', 'Año', 'Pizzas vendidas por mes', fila)
     return excel
 
 
-def escribir_excel(ventas_tipos_pizza, ventas_dia_semana, ventas_mes, ventas_hora):
+def hoja2(excel: xlsxwriter.Workbook, ingredientes_usados: pd.DataFrame):
+    worksheet = excel.add_worksheet('reporte_de_ingredientes')
+    worksheet.merge_range(0, 0, 0, 2, 'Catalina Royo-Villanova Seguí')
+    titulo1 = excel.add_format({'bold': True, 'font_size':20, 'font_color':'red', 'align':'center', 'border': 5})
+    worksheet.merge_range(1, 1, 1, 14, "Reporte de Ingredientes de Maven's Pizza".upper(), titulo1)
+    titulo2 = excel.add_format({'bold': True, 'font_size':16, 'font_color':'blue', 'align':'center', 'border': 2})
+    worksheet.merge_range(2, 1, 2, 14, 'Ingredientes más y menos usados', titulo2)
+    columnas = list(ingredientes_usados.columns)
+    titulo_celda = excel.add_format({'bold': True, 'border': 1})
+    celda_verde = excel.add_format({'font_color':'28B463', 'border': 1})
+    celda_roja = excel.add_format({'font_color':'DF1616', 'border': 1})
+    celda_azul = excel.add_format({'font_color':'16B1DF', 'border': 1})
+    media = ingredientes_usados[columnas[1]].mean()
+
+    worksheet.write(4, 1, columnas[0], titulo_celda)
+    worksheet.write(4, 2, columnas[1], titulo_celda)
+    for j in range(len(ingredientes_usados)):
+        if ingredientes_usados.loc[j, columnas[1]] > media:
+            worksheet.write(j+5, 1, ingredientes_usados.loc[j, columnas[0]], celda_verde)
+            worksheet.write(j+5, 2, ingredientes_usados.loc[j, columnas[1]], celda_verde)
+        elif ingredientes_usados.loc[j, columnas[1]] < media:
+            worksheet.write(j+5, 1, ingredientes_usados.loc[j, columnas[0]], celda_roja)
+            worksheet.write(j+5, 2, ingredientes_usados.loc[j, columnas[1]], celda_roja)
+        else:
+            worksheet.write(j+5, 1, ingredientes_usados.loc[j, columnas[0]], celda_azul)
+            worksheet.write(j+5, 2, ingredientes_usados.loc[j, columnas[1]], celda_azul)
+
+    worksheet.merge_range(4, 4, 69, 14, '', celda_verde)
+    worksheet.insert_image('E5', 'ingredientes_usados.png', {'x_scale': 0.99, 'y_scale': 1.38, 'x_offset': 1, 'y_offset': 1})
+    return excel
+
+
+def hoja3(excel: xlsxwriter.Workbook, ventas_tipos_pizza: pd.DataFrame, ventas_tamanos_pizza: pd.DataFrame):
+    worksheet = excel.add_worksheet('reporte_de_pedidos')
+    worksheet.merge_range(0, 0, 0, 2, 'Catalina Royo-Villanova Seguí')
+    titulo1 = excel.add_format({'bold': True, 'font_size':20, 'font_color':'red', 'align':'center', 'border': 5})
+    worksheet.merge_range(1, 1, 1, 14, "Reporte de Pedidos de Maven's Pizza".upper(), titulo1)
+    worksheet, fila = escribir_tabla_y_grafico(excel, worksheet, ventas_tipos_pizza, 'reporte_de_pedidos', 'tipos', "Tipos de pizzas más y menos vendidos", 0)
+    worksheet, fila = escribir_tabla_y_grafico(excel, worksheet, ventas_tamanos_pizza, 'reporte_de_pedidos', 'tamaños', "Tamaños de pizzas más y menos vendidos", fila)
+    return excel
+
+
+def escribir_excel(ventas_dia_semana, ventas_mes, ventas_hora, ingredientes_usados, ventas_tipos_pizza, ventas_tamanos_pizza):
     excel = xlsxwriter.Workbook('Reporte_Ejecutivo_COO.xlsx')
-    excel = hoja(excel, ventas_hora, 'horas', 'Pizzas vendidas por hora', 'Hora')
-    excel = hoja(excel, ventas_dia_semana, 'días_semana', 'Pizzas vendidas por día de la semana', 'Día de la semana')
-    excel = hoja(excel, ventas_mes, 'meses', 'Pizzas vendidas por mes', 'Mes')
-    excel = hoja(excel, ventas_tipos_pizza, 'tipos_de_pizza', "Tipos de pizzas más y menos vendidos", 'Tipos')
+    excel = hoja1(excel, ventas_hora, ventas_dia_semana, ventas_mes)
+    excel = hoja2(excel, ingredientes_usados)
+    excel = hoja3(excel, ventas_tipos_pizza, ventas_tamanos_pizza)
     excel.close()
 
 
@@ -78,6 +123,6 @@ if __name__ == '__main__':
     nombres = ['order_details', 'orders', 'pizza_types']
     separadores = [';', ';', ',']
     order_details, orders, pizza_types = Analizar_datos_2016.Extract(nombres, separadores)
-    date_order_details = Analizar_datos_2016.Transform(order_details, orders)
-    ventas_tipos_pizza, ventas_dia_semana, ventas_mes, ventas_hora = Analizar_datos_2016.Load(pizza_types, date_order_details)
-    escribir_excel(ventas_tipos_pizza, ventas_dia_semana, ventas_mes, ventas_hora)
+    date_order_details = Analizar_datos_2016.Transform(pizza_types, order_details, orders)
+    ventas_dia_semana, ventas_mes, ventas_hora, ingredientes_usados, ventas_tipos_pizza, ventas_tamanos_pizza = Analizar_datos_2016.Load(pizza_types, date_order_details)
+    escribir_excel(ventas_dia_semana, ventas_mes, ventas_hora, ingredientes_usados, ventas_tipos_pizza, ventas_tamanos_pizza)
